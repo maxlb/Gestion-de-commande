7d112b9ee4edb331d7139573b83c832b6ac9b2ff
Maxime LB <maxime.lebail@orange.fr>
2017-03-22 13:38:40 +0100

    init

---
+++ Commande/controleur/CommandeControleur.cs
@@ -0,0 +1,51 @@
+﻿using GestionCommande.dao;
+using GestionCommande.model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCommande.controleur
+{
+    class CommandeControleur:Controleur
+    {
+        private ClientDao ClientDao { get; }
+        private ProduitDao ProduitDao { get; }
+        private CommandeDao CommandeDao { get; }
+
+        public CommandeControleur()
+        {
+            this.ClientDao = new ClientDao();
+            this.ProduitDao = new ProduitDao();
+            this.CommandeDao = new CommandeDao();
+        }
+
+        public void CreerCommande(Client client,ICollection<LigneCommande> ligneCmd)
+        {
+            Commande cmd = new Commande { Id = CommandeDao.Commandes.Count + 1, Client = client, LignesCommande = ligneCmd };
+            foreach (LigneCommande ligne in ligneCmd)
+            {
+                ligne.Commande = cmd;
+            }
+            client.Commandes.Add(cmd);
+            CommandeDao.AjouterCommande(cmd);
+        }
+
+        public ICollection<Client>  GetClients()
+        {
+            return ClientDao.Clients;
+        }
+
+        public ICollection<Produit> GetProduits()
+        {
+            return ProduitDao.Produits;
+        }
+
+        public ICollection<Commande> GetCommandes()
+        {
+            return CommandeDao.Commandes;
+        }
+    }
+}
---
+++ Commande/controleur/Controleur.cs
@@ -0,0 +1,21 @@
+﻿using GestionCommande.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCommande.controleur
+{
+    public interface Controleur
+    {
+
+        void CreerCommande(Client client, ICollection<LigneCommande> ligneCmd);
+
+        ICollection<Client> GetClients();
+
+        ICollection<Produit> GetProduits();
+
+        ICollection<Commande> GetCommandes();
+    }
+}
